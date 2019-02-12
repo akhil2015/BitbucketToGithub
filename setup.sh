cd9ebbdc9ceee1a2c8e06e2b49cd4945b07727bb
Akhil Bharti <akhil.bh96@gmail.com>
2019-02-11 20:32:13 -0800

    added install script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo "## This script will install dependencies for BitbucketToGithub"
+echo "## run with 'sudo' if it doesn't work"
+echo ""
+echo ""
+
+pip3 install requests
+pip3 install pprintpp
+pip3 install pygithub
